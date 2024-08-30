38ff2f9f591af1f150d3b62014deaa51eeb92261
RoycetheGreat2 <165749717+RoycetheGreat2@users.noreply.github.com>
2024-08-30 05:42:17 +0000

    zxcasd

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class welcome{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to CS 211: Object-oriented Programming");
+        System.out.print("Name: ");
+        string name = input.nextLine();
+        System.out.println(name + ", This course will be fun.");
+    }
+}
